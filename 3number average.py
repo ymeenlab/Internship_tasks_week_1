318b84af8397e2a88884eb302f6b27afa09b555f
YmeenFatima <ymeenfatima19@gmail.com>
2025-07-31 18:42:27 +0500

    average

---
+++ 3number average.py
@@ -0,0 +1,8 @@
+# Task: 3 numbers and their average
+
+A = int(input('enter first number: '))
+B= int(input('enter second number:'))
+C= int(input('enter third number:'))
+Avg= (A+B+C)/3
+print('The average of three numbers is: ', Avg)
+
